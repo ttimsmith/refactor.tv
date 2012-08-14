e266456235403e7c67a144719ebda91eaa4d8538
Sam Soffes <sam@samsoff.es>
2012-08-14 08:57:16 -0700

    Fix feed

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,7 @@
 class FeedsController < ApplicationController
   def show
-    @episode_files = EpisodeFile.where(file_type: params[:file_type])
+    @feed_type = params[:file_type]
+    @episode_files = EpisodeFile.where(file_type: @feed_type)
     if @episode_files.blank?
       head :not_found and return
     end

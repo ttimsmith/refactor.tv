dfe5ad7cd00286def3dc2eb9458376236f37f3c1
Sam Soffes <sam@samsoff.es>
2012-08-28 20:12:25 -0700

    Start on redesign

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,26 +1,30 @@
 class Episode < ActiveRecord::Base
   attr_accessible :notes, :position, :published_at, :seconds, :title, :summary
-  
+
   has_many :episode_files
-  
+
   validates_presence_of :position, :title, :summary, :notes, :seconds
   validates_uniqueness_of :position, :title
-  
+
   scope :published, lambda { where('published_at <= ?', Time.now.utc) }
   scope :unpublished, lambda { where('published_at > ?', Time.now.utc) }
   scope :recent, order('position DESC')
-  
+
   before_save :update_notes_html
-  
+
   def to_param
     self.position
   end
-  
+
+  def padded_position
+    self.position.to_s.rjust(3, '0')
+  end
+
 private
 
   def update_notes_html
     return if self.notes.blank?
-    
+
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
       no_intra_emphasis: true,
       tables: true,

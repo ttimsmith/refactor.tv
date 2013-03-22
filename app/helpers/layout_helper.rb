0cbb652cc3563a142035fdb3d239e48858c11f94
Sam Soffes <sam@soff.es>
2013-03-21 23:07:25 -0400

    Use Google Analytics

--- app/helpers/layout_helper.rb
+++ app/helpers/layout_helper.rb
@@ -11,10 +11,6 @@ module LayoutHelper
     content_for(:head) { javascript_include_tag(*args) }
   end
 
-  def gauges(code)
-    %Q{<script type="text/javascript">var _gauges=_gauges||[];(function(){var t=document.createElement('script');t.async=true;t.id='gauges-tracker';t.setAttribute('data-site-id','#{code}');t.src='//secure.gaug.es/track.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(t, s);})();</script>}.html_safe if Rails.env.production?
-  end
-
   def follow_button(username, options = {})
     options_string = options.map { |key, value| "data-#{key}=\"#{value}\"" }.join(' ')
     options_string = " #{options_string}" if options_string.length > 0
